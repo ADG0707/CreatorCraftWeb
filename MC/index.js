f2451845e51ab0a5d80b425f746993c01afb4fef
Adrian Gustafsson <adrian.gustafsson1@learnet.se>
2024-01-31 08:27:02 +0100

    newapi

--- MC/index.js
+++ MC/index.js
@@ -1,32 +1,31 @@
 
 
-function DropClick(deg,a) {
+function DropClick(deg, a) {
   if (a == false) {
     var List = document.getElementById("Drop_ListBar")
     var DripImg = document.getElementById("Nav_I_Drop_Img");
     if (List.style.display == "none") {
-        DripImg.style.transform = "rotate(180deg)";
-        List.style.display = "flex";
-        List.style.height = "500px";
+      DripImg.style.transform = "rotate(180deg)";
+      List.style.display = "flex";
+      List.style.height = "500px";
 
-    }else
-     {
-        DripImg.style.transform = "rotate(0deg)";
-        List.style.display = "none";
-        List.style.height = "0px";
+    } else {
+      DripImg.style.transform = "rotate(0deg)";
+      List.style.display = "none";
+      List.style.height = "0px";
     };
   }
-  
 
 
 
-  if(a == true){
+
+  if (a == true) {
     Button = document.getElementById("RuleButton");
     RulesPage = document.getElementById("RulePage");
     console.log(RulesPage, RulesPage.style.hidden)
-    if(RulesPage.style.display == "none"){
+    if (RulesPage.style.display == "none") {
       RulesPage.style.display = "flex"
-    }else{
+    } else {
       RulesPage.style.display = "none"
     }
   }
@@ -43,40 +42,87 @@ function DropClick(deg,a) {
 
 
 //based on a pen by @robinselmer
-var url = "https://api.minetools.eu/ping/CreatorCraftWeb.undo.it	"; //insert  server here
-$.getJSON(url, function(r) {
+var url = "https://api.mcsrvstat.us/3/creatorcraftmc.us.to	"; //insert  server here
+$.getJSON(url, function (r) {
   //data is the JSON string
   if (r.error) {
     $('#rest').html('Server Offline');
     return false;
   }
   console.log(r)
-  
-  if (r.players.sample.length > 0) {
-    for (let index = 0; index < r.players.sample.length; index++) {
-      var id = r.players.sample[index].id
+
+  if (r.players.online > 0 && r.players.list.length > 0) {
+    for (let index = 0; index < r.players.list.length; index++) {
+      var id = r.players.list[index].uuid
       var li = document.createElement("li");
-      li.innerHTML = r.players.sample[index].name;
-      li.id = r.players.sample[index].name
+      li.innerHTML = r.players.list[index].name;
+      li.id = r.players.list[index].name
       li.className = "PLi"
       document.getElementById("PlistCont").append(li);
-      
+
       var im2 = document.createElement("img")
-      
+
       im2.src = "https://api.mineatar.io/body/full/" + id;
       //img.src = "https://api.mineatar.io/skin/" + id
       im2.id = "im2"
-      
-      document.getElementById(r.players.sample[index].name).append(im2);
-      
-      
 
-      
+      document.getElementById(r.players.list[index].name).append(im2);
+
+
+
+
+    }
   }
+  function occurrences(string, subString, allowOverlapping) {
+
+    string += "";
+    subString += "";
+    if (subString.length <= 0) return (string.length + 1);
+
+    var n = 0,
+      pos = 0,
+      step = allowOverlapping ? 1 : subString.length;
+
+    while (true) {
+      pos = string.indexOf(subString, pos);
+      if (pos >= 0) {
+        ++n;
+        pos += step;
+      } else break;
+    }
+    return n;
   }
-  
-  $('#SPMb').html( '<br><b>Players Online:</b> ' + r.players.online);
-  $('#SIM').attr('src', r.favicon);
-  
-  $('#Desc').html(r.description)
+  $('#SPMb').html('<br><b>Players Online:</b> ' + r.players.online);
+  $('#SIM').attr('src', r.icon);
+  var Sname = r.motd.clean[0]
+  var ht = r.motd.html[0]
+  var colors = []
+  for (let i = 0; i < occurrences(r.motd.html, "color:"); i++) {
+    colors.push(ht.slice(ht.indexOf("color:") + 7, (ht.indexOf("color:") + 14)))
+    ht = ht.replace(ht.slice(ht.indexOf("color:"), (ht.indexOf("color:") + 7)), " ")
+    console.log()
+  }
+  console.log(occurrences(ht, "color"))
+  console.log(colors)
+  var Creator = Sname.slice(0, 7)
+  var Craft = Sname.slice(7, 13)
+  var Minecraft = Sname.slice(24)
+  var Modded = Sname.slice(17,24)
+  var CrB = document.createElement("b")
+  var CB = document.createElement("b")
+  var MB = document.createElement("b")
+  var MD = document.createElement("b")
+  CrB.style = "color: " + colors[0]
+  CB.style = "color: " + colors[1]
+  MB.style = "color: " + colors[2]
+  MD.style = "color: " + colors[4]
+  CrB.innerHTML = Creator
+  CB.innerHTML = Craft
+  MB.innerHTML = Minecraft
+  MD.innerHTML = Modded
+
+  $('#SNb').append(CrB, CB,MD, MB)
 });
+//kjasdnkasjhdiuashd
+
+
