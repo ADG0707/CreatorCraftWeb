51eb9337066a11e051b7a608db90c550594a3223
ADG0707 <62938080+ADG0707@users.noreply.github.com>
2024-03-22 14:02:56 +0100

    Rework Php

--- MC/BAck/db.inc.php
+++ MC/BAck/db.inc.php
@@ -1,12 +1,12 @@
 
 <?php
-    $dsn = "mysql:host=sql11.freesqldatabase.com;dbname=sql11691707;";
-    $dbusername = "sql11691707";
-    $dbpassword = "CEiHqdguHV";
+    $dsn = "mysql:localhost;port=3306;dbname=asql;";
+    $dbusername = "root";
+    $dbpassword = "TgX42&j!";
     try {
         $pdo = new PDO($dsn,$dbusername,$dbpassword);
         $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         
     } catch (PDOException $e) {   
-        echo "Failed: ", $e;
+        echo "Failed: ", phpinfo().pdo_drivers();
     }
